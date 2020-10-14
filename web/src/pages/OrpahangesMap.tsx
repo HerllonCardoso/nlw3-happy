30c3c15a37bdccefe8bc1bcb3de65a5f4c268b9a
Herllon Cardoso <herllon.cardoso@alfalaval.com>
2020-10-14 13:08:23 -0300

    feat: leaflet map

--- web/src/pages/OrpahangesMap.tsx
+++ web/src/pages/OrpahangesMap.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi';
+import {Map, TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMakerImg from '../images/map-maker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+        <img src={mapMakerImg} alt="Happy"/>
+        
+        <h2>Escolha um orfanato no mapa</h2>
+        <p>Muitas crianças estão esperando
+          a sua visita
+        </p>
+        </header>
+
+        <footer>
+          <strong>Petrópolis</strong>
+          <span>Rio de Janeiro</span>
+        </footer>
+        
+      </aside>
+
+      <Map 
+        center={[-22.3843617,-43.1739833]}
+        zoom={10.75}
+        style={{ width:'100%', height:'100%'}}
+        >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  )
 }
 
 export default OrphanagesMap;
