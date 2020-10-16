6243e652208d42a125fc9cd9c530afa7d36d8efc
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-10-15 23:21:46 -0300

    feat: index/show/create routes

--- backend/src/database/migrations/1602776237658-create_orphanages.ts
+++ backend/src/database/migrations/1602776237658-create_orphanages.ts
@@ -1,54 +1,58 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createOrphanages1602776237658 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(new Table({
-        name: 'orpahanges',
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "orphanages",
         columns: [
           {
-            name: 'id',
-            type: 'integer',
+            name: "id",
+            type: "integer",
             unsigned: true,
             isPrimary: true,
             isGenerated: true,
-            generationStrategy: 'increment',
+            generationStrategy: "increment",
           },
           {
-            name: 'name',
-            type: 'varchar'
+            name: "name",
+            type: "varchar",
           },
           {
-            name: 'latitude',
-            type: 'decimal',
+            name: "latitude",
+            type: "decimal",
             scale: 10,
             precision: 2,
           },
           {
-            name: 'longitude',
-            type: 'decimal',
+            name: "longitude",
+            type: "decimal",
             scale: 10,
             precision: 2,
           },
           {
-            name: 'about',
-            type: 'text',
+            name: "about",
+            type: "text",
           },
           {
-            name: 'instructions',
-            type: 'text'
+            name: "instructions",
+            type: "text",
           },
           {
-            name: 'open_on_weekends',
-            type: 'boolean',
-            default: false
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
+            name: "open_on_weekends",
+            type: "boolean",
+            default: false,
           },
         ],
-      }))
-    }
+      })
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable('orphanages');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("orphanages");
+  }
 }
