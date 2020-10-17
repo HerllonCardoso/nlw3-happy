60fea453c68f4ba358842b9b75e3da2ff20d3778
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-10-16 23:24:06 -0300

    fix: using open street map as tyler for production environment

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -46,10 +46,10 @@ function OrphanagesMap() {
         zoom={11.75}
         style={{ width: "100%", height: "100%" }}
       >
-        {/*   <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-        />
+        /> */}
 
         {orphanages.map((orphanage) => {
           return (
